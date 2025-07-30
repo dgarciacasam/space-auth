3099cf6624330afbf1f3c71dfb09672100ddd854
dgarciacasam <dgarciacasam@gmail.com>
2025-07-30 23:26:01 +0200

    Added testing to auth service

--- src/main/java/com/dgarciacasam/authService/Models/LoginRequest.java
+++ src/main/java/com/dgarciacasam/authService/Models/LoginRequest.java
@@ -1,9 +1,11 @@
 package com.dgarciacasam.authService.Models;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LoginRequest {
     @NotBlank(message = "El usuario es obligatorio")
     private String username;
--- src/main/java/com/dgarciacasam/authService/Models/RegisterRequest.java
+++ src/main/java/com/dgarciacasam/authService/Models/RegisterRequest.java
@@ -2,9 +2,11 @@ package com.dgarciacasam.authService.Models;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class RegisterRequest {
     @NotBlank(message = "El nombre de usuario es obligatorio")
     private String username;
